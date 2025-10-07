440a5a4c5c5ec5d789f29ec6b0882490c8d70aa8
Michał Wachowicz <michalwachowicz@Michas-MacBook-Pro.local>
2025-10-07 10:00:42 +0200

    feat: create folder route

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,3 @@
 export * as authController from "./auth-controller";
+export * as folderController from "./folder-controller";
 export * as rootController from "./root-controller";
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,8 +1,10 @@
 import { Express } from "express";
 import authRouter from "./auth-router";
+import folderRouter from "./folder-router";
 import rootRouter from "./root-router";
 
 export function initializeRouters(app: Express) {
   app.use("/auth", authRouter);
+  app.use("/folder", folderRouter);
   app.use("/", rootRouter);
 }
